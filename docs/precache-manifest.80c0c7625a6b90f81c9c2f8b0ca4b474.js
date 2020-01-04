704de1f195e1c76f5333e3252bdd2f2d56207501
marinadelafuente <mdelafuente18@gmail.com>
2020-01-04 12:49:36 +0100

    add docs and create-react-app

---
+++ docs/precache-manifest.80c0c7625a6b90f81c9c2f8b0ca4b474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c02b8b82d1b610b87f4b9e2f7da13b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a196f9147a620b01f677",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9358749e06f00f6ae84e",
+    "url": "./static/js/2.1a3b3076.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.1a3b3076.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a196f9147a620b01f677",
+    "url": "./static/js/main.e7b3d856.chunk.js"
+  },
+  {
+    "revision": "cf5a737ff533e59b7a9c",
+    "url": "./static/js/runtime-main.626423cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
