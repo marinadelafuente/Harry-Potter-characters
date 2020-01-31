93d76c6121fa91d303202e2e04389b18adf2c5d1
marinadelafuente <mdelafuente18@gmail.com>
2020-01-31 13:50:05 +0100

    add updated docs

---
+++ docs/precache-manifest.ecbd87e94b7ba82a82e98085946eaac4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475c76e2f38b7c76711d4cf85cd98525",
+    "url": "./index.html"
+  },
+  {
+    "revision": "823eb439c7ce1f58d4a1",
+    "url": "./static/css/main.5e08a8e1.chunk.css"
+  },
+  {
+    "revision": "2484bde33ca3a05b0c8e",
+    "url": "./static/js/2.2ef6d4f8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.2ef6d4f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "823eb439c7ce1f58d4a1",
+    "url": "./static/js/main.011ae545.chunk.js"
+  },
+  {
+    "revision": "cf5a737ff533e59b7a9c",
+    "url": "./static/js/runtime-main.626423cf.js"
+  },
+  {
+    "revision": "5a705219bf23914e38fe16c30ec36fd8",
+    "url": "./static/media/Gryffindor.5a705219.png"
+  },
+  {
+    "revision": "026c8b2e0f869ef29492980b74e3e4fc",
+    "url": "./static/media/Hufflepuff.026c8b2e.png"
+  },
+  {
+    "revision": "bf5d2f7cac2a83a0d8414281f7b615ae",
+    "url": "./static/media/Ravenclaw.bf5d2f7c.png"
+  },
+  {
+    "revision": "d3bf2fd813f2c8b592629730222b5801",
+    "url": "./static/media/Slytherin.d3bf2fd8.png"
+  },
+  {
+    "revision": "5a5afce4b46ce8a19a6e8b293e165f8b",
+    "url": "./static/media/frog-cards.5a5afce4.png"
+  },
+  {
+    "revision": "c7916ae822170272bf7fe0e5ec8356cd",
+    "url": "./static/media/golden_snitch.c7916ae8.png"
+  },
+  {
+    "revision": "a78f40ca99d5fb3dfe82538baea15037",
+    "url": "./static/media/logo.a78f40ca.png"
+  },
+  {
+    "revision": "e0804e94b61926e195e45a336ce372c4",
+    "url": "./static/media/pergamin.e0804e94.jpg"
+  }
+]);
